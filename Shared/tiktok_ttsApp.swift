3e6863c0ffd744d9050784533969fb840e5a99a8
Sophie Luna Schumann <me@luna.vg>
2022-05-12 14:46:35 +0200

    Initial Commit

---
+++ Shared/tiktok_ttsApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiktok_ttsApp.swift
+//  Shared
+//
+//  Created by Sophie Luna Schumann on 12.05.22.
+//
+
+import SwiftUI
+
+@main
+struct tiktok_ttsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
